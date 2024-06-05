c215e3784a47517b30d48097944cd8e43b5a5bbc
Jefferson Duarte <jeffersonsduarte@live.com>
2024-06-05 18:53:57 +0100

    Created models category and contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Contact, Category
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'created_date',
+    ]
+    ordering = ['-id']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
