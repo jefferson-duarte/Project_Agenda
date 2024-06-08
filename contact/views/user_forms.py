8060a7cba864ce7fe98f6210ceaa2d79bd1b5d63
Jefferson Duarte <jeffersonsduarte@live.com>
2024-06-08 09:45:59 +0100

    Created function to login and logout users

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,8 @@
+from django.contrib import auth
 from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register(request):
@@ -15,7 +17,7 @@ def register(request):
                 request,
                 'User created successfully.'
             )
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     context = {
         'form': form,
@@ -26,3 +28,44 @@ def register(request):
         'contact/register.html',
         context
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(
+                request,
+                'Logged in successfully.'
+            )
+
+            return redirect('contact:index')
+        messages.error(
+            request,
+            'Login invalid.'
+        )
+
+    context = {
+        'form': form,
+    }
+
+    return render(
+        request,
+        'contact/login.html',
+        context
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+
+    messages.success(
+        request,
+        'Logged out successfully.'
+    )
+    return redirect('contact:login')
